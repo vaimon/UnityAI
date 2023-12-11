e7320bac90bbafe1d56a2d32d93c0acd771401a2
RobolabGs2 <slavikelk@yandex.ru>
2023-12-11 19:30:17 +0300

    Lab 02

---
+++ Assets/Scripts/AgentPusher.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+
+public class AgentPusher : Agent
+{
+    float m_LateralSpeed = 0.15f;
+    float m_ForwardSpeed = 0.5f;
+
+
+    [HideInInspector]
+    public Rigidbody agentRb;
+
+
+    public override void Initialize()
+    {
+        agentRb = GetComponent<Rigidbody>();
+        agentRb.maxAngularVelocity = 500;
+    }
+
+    public void MoveAgent(ActionSegment<int> act)
+    {
+        var dirToGo = Vector3.zero;
+        var rotateDir = Vector3.zero;
+
+
+        var forwardAxis = act[0];
+        var rightAxis = act[1];
+        var rotateAxis = act[2];
+
+        switch (forwardAxis)
+        {
+            case 1:
+                dirToGo = transform.forward * m_ForwardSpeed;
+                break;
+            case 2:
+                dirToGo = transform.forward * -m_ForwardSpeed;
+                break;
+        }
+
+        switch (rightAxis)
+        {
+            case 1:
+                dirToGo = transform.right * m_LateralSpeed;
+                break;
+            case 2:
+                dirToGo = transform.right * -m_LateralSpeed;
+                break;
+        }
+
+        switch (rotateAxis)
+        {
+            case 1:
+                rotateDir = transform.up * -1f;
+                break;
+            case 2:
+                rotateDir = transform.up * 1f;
+                break;
+        }
+
+        transform.Rotate(rotateDir, Time.deltaTime * 100f);
+        agentRb.AddForce(dirToGo, ForceMode.VelocityChange);
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        MoveAgent(actionBuffers.DiscreteActions);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        //forward
+        if (Input.GetKey(KeyCode.W))
+        {
+            discreteActionsOut[0] = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            discreteActionsOut[0] = 2;
+        }
+        //rotate
+        if (Input.GetKey(KeyCode.A))
+        {
+            discreteActionsOut[2] = 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            discreteActionsOut[2] = 2;
+        }
+        //right
+        if (Input.GetKey(KeyCode.E))
+        {
+            discreteActionsOut[1] = 1;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            discreteActionsOut[1] = 2;
+        }
+    }
+
+    public override void OnEpisodeBegin()
+    {
+    }
+
+}
---
+++ Assets/Scripts/GameEnvController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+public class GameEnvController : MonoBehaviour
+{
+    public int buttonsOnEpisode = 4;
+    public int boxesOnEpisode = 3;
+
+    private Agent agent;
+    public GridedDistributor buttonsDistributor;
+    public GridedDistributor boxDistributor;
+    public GridedDistributor agentsDistributor;
+    public Door door;
+    public MeshCollider goal;
+
+    void Start()
+    {
+        ResetScene();
+    }
+
+    void ResetScene()
+    {
+        var buttons = buttonsDistributor.Respawn(buttonsOnEpisode);
+        boxDistributor.Respawn(boxesOnEpisode);
+        var activators = new DoorActivator[buttons.Length];
+        for (var i = 0; i < buttons.Length; i++)
+            activators[i] = buttons[i].GetComponent<Button>();
+        door.ResetActivators(activators);
+
+        agent = agentsDistributor.Respawn(1)[0].GetComponent<Agent>();
+    }
+
+    public void OnGoalTriggered()
+    {
+        agent.EndEpisode();
+        ResetScene();
+    }
+    void FixedUpdate()
+    {
+    }
+}
