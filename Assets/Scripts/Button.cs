d0feacd5a91eaa822809e5c1b441fd770019bb24
Andrew Nikolaev <Andrew.Vaimon@yandex.ru>
2024-01-19 19:04:14 +0300

    Update rewards distribution

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+
+using Unity.MLAgents;
+
 using UnityEngine;
 
 public enum ButtonStates {
@@ -27,6 +30,9 @@ public class Button : ButtonActivator
     void OnCollisionEnter(Collision collision) {
         collisionsCount++;
         if (state == ButtonStates.Unpressed) {
+            var agent = collision.gameObject.GetComponent<Agent>();
+            agent.AddReward(0.1f);
+            tag = "pressedButton";
             state = ButtonStates.Pressed;
             mesh.material = pressedMaterial;
             onActivate.Invoke();
@@ -35,6 +41,9 @@ public class Button : ButtonActivator
     void OnCollisionExit(Collision collision) {
         collisionsCount--;
         if (collisionsCount <= 0 && state == ButtonStates.Pressed) {
+            tag = "button";
+            var agent = collision.gameObject.GetComponent<Agent>();
+            agent.AddReward(-0.1f);
             state = ButtonStates.Unpressed;
             mesh.material = unpressedMaterial;
             onDeactivate.Invoke();
